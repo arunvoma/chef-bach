f264711620086cc2f5dcc776df653dee51990bf1
Arun Voma <avoma@hw11190.local>
2015-05-12 10:53:18 -0400

    Modified Ambari view recipe to use the variables

--- cookbooks/bcpc-hadoop/recipes/ambari.rb
+++ cookbooks/bcpc-hadoop/recipes/ambari.rb
@@ -1,14 +1,13 @@
 
-bash "download_repo" do
-code <<EOH
-	wget -nv http://public-repo-1.hortonworks.com/ambari/ubuntu12/2.x/updates/2.0.0/ambari.list -O /etc/apt/sources.list.d/ambari.list
-	apt-key adv --recv-keys --keyserver
-    wget http://public-repo-1.hortonworks.com/HDP/ubuntu12/2.x/updates/2.2.4.2/hdp.list -O /etc/apt/sources.list.d/hdp.list
-	apt-get update
-	apt-get install ambari-server
-	apt-get install tez
-EOH
-end
+ambari_repo_url = "http://public-repo-1.hortonworks.com/ambari/ubuntu12/2.x/updates/2.0.0/ambari.list"
+hdp_repo_url = "http://public-repo-1.hortonworks.com/HDP/ubuntu12/2.x/updates/2.2.4.2/hdp.list"
+repo_dir = "/etc/apt/sources.list.d"
+ambari_server_ip="192.168.100.12"
+ambari_uri="http://#{ambari_server_ip}:8080/api/v1/views/TEZ/versions/0.5.2.2.2.2.0-151/instances"
+instance_name="Tez1"
+yarn_timelineserver_url="http://10.0.100.12:8188"
+yarn_resourcemanager_url="http://192.168.100.12:8088"
+
 
 %w{ambari-server tez postgresql}.each do |pkg|
   package pkg do
@@ -22,16 +21,16 @@ end
 
 execute "setup ambari-server" do
   command "ambari-server setup -s"
-  not_if { ::File.exists?("/etc/ambari-server/conf/ambari.properties")}
 end
 
-service "ambari-server" do
-action [:enable, :start]
+#Starting multiple services in one block
+
+%w{ambari-server postgresql}.each do |pkg|
+service pkg do
+  action [:enable, :start]
+end
 end
 
-service "postgresql" do
-  action [ :enable, :start ]
-end
 
 template "/etc/init.d/hadoop-yarn-timelineserver" do
   source "hdp_hadoop-yarn-timelineserver-initd.erb"
@@ -47,7 +46,7 @@ end
 
 
 execute "create tez view instance" do
-  command "curl -H \"X-Requested-By:ambari\" -u admin:admin -X POST -d '[{ \"ViewInstanceInfo\" : { \"label\" : \"Tez custom View\", \"properties\" : {\"yarn.timeline-server.url\" : \"http:/10.0.100.13:8188\", \"yarn.resourcemanager.url\" : \"http://192.168.100.13:8088\" } } } ]' http://192.168.100.13:8080/api/v1/views/TEZ/versions/0.5.2.2.2.2.0-151/instances/Tez1"
-  only_if {"curl -s -H \"X-Requested-By:ambari\" -u admin:admin -X GET http://192.168.100.13:8080/api/v1/views/TEZ/versions/0.5.2.2.2.2.0-151/instances/Tez1 | grep instance_name | tr -d \", | head -1 | awk -F ':' '{print $2}'" != "Tez1"}
-  notifies :restart, 'service[ambari-server]', :immediately
+command "curl -H \"X-Requested-By:ambari\" -u admin:admin -X POST -d '[{ \"ViewInstanceInfo\" : { \"label\" : \"Tez custom View\", \"properties\" : {\"yarn.timeline-server.url\" : \"#{yarn_timelineserver_url}\", \"yarn.resourcemanager.url\" : \"#{yarn_resourcemanager_url}\" } } } ]' #{ambari_uri}/#{instance_name}"
+only_if {"curl -s -H \"X-Requested-By:ambari\" -u admin:admin -X GET #{ambari_uri}/#{instance_name} | grep instance_name | tr -d \", | head -1 | awk -F ':' '{print $2}'|tr -d ' '" != "#{instance_name}"}
+notifies :restart, 'service[ambari-server]', :immediately
 end
