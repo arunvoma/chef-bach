577a7110211da6cff88c9fd3a111275f640dd6f5
Arun Voma <avoma@hw11190.local>
2015-06-22 12:11:05 -0400

    Build Ambari server

---
+++ cookbooks/ambari-server/README.md
@@ -0,0 +1,5 @@
+ambari-server Cookbook
+======================
+This cookbook builds ambari server package without posgressql dependency and deploys the  ambari server package on a given host and creates a TEZ view instance.
+TEZ View instance provides graphical view of TEZ jobs with all the job properties provided the Application TimelineServer.
+
---
+++ cookbooks/ambari-server/recipes/ambari_server_build.rb
@@ -0,0 +1,48 @@
+
+
+ark "ambari_server" do
+url node[:ambari][:tar][:url]
+version '2.0.1'
+path "/tmp"
+action :put
+notifies :run,"bash[extracting_package]", :immediately
+#not_if File.exists?(node[:ambari][:bins_dir]/ambari-server-withoutdependencies.deb)
+not_if { ::File.exists?(node[:ambari][:install_dir])}
+end
+
+
+bash "extracting_package" do
+cwd node[:ambari][:install_dir]
+code <<EOH
+dpkg-deb -x "#{node[:ambari][:deb_path]}" "#{node[:ambari][:temp_dir]}"
+dpkg-deb --control "#{node[:ambari][:deb_path]}" "#{node[:ambari][:temp_dir]}/DEBIAN"
+#sed -i 's/, postgresql (>= 8.1)//' "#{node[:ambari][:temp_dir]}/DEBIAN/control"
+EOH
+notifies :run,"bash[Editing_Control_File]", :immediately
+#notifies :run,"ruby_block[edit_control_file]", :immediately
+end
+
+bash "Editing_Control_File" do
+code <<EOH
+sed -i 's/, postgresql (>= 8.1)//' "#{node[:ambari][:temp_dir]}/DEBIAN/control"
+EOH
+only_if { ::File.exists?("#{node[:ambari][:temp_dir]}/DEBIAN/control")}
+end
+
+
+
+bash 'building package' do
+code <<EOH
+dpkg -b #{node[:ambari][:temp_dir]} #{node[:ambari][:new_deb_path]}
+cp #{node[:ambari][:new_deb_path]} #{node[:ambari][:bins_dir]}
+rm -rf  #{node[:ambari][:temp_dir]} #{node[:ambari][:new_deb_path]} #{node[:ambari][:install_dir]}
+EOH
+end
+
+bash "Executing Build Bins Script" do
+cwd /home/vagrant/chef-bcpc
+code <<EOH
+./build_bins.sh 
+EOH
+end
+
